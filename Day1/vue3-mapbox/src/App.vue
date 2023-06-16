d18fa498c14f82f219ada5f0b92f30e1960506bc
Silver Str1ng <rakuyou63@gmail.com>
2023-06-16 16:08:51 +0800

    vue+mapbox

---
+++ Day1/vue3-mapbox/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="map">
+
+  </div>
+</template>
+
+<script setup>
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { onMounted } from 'vue';
+const token = import.meta.env.VITE_MAPBOX_TOKEN;
+mapboxgl.accessToken = token;
+onMounted(() => {
+  const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+  });
+})
+</script>
+
+<style lang="scss" scoped>
+#map {
+  width: 100%;
+  height: 100vh;
+}
+</style>
